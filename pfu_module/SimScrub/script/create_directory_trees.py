5d417ed5e9bd2e832228c101ddcce0b0d0ddbe96
Daniel Mohr <daniel.mohr@dlr.de>
2021-05-23 14:17:14 +0200

    fixed pfu_module/SimScrub/script/create_directory_trees.py (pfu.py simscrub should work now)

--- pfu_module/SimScrub/script/create_directory_trees.py
+++ pfu_module/SimScrub/script/create_directory_trees.py
@@ -1,7 +1,7 @@
 """
 :Author: Daniel Mohr
 :Email: daniel.mohr@gmx.de
-:Date: 2017-01-29 (last change).
+:Date: 2017-01-29, 2021-05-23 (last change).
 :License: GNU GENERAL PUBLIC LICENSE, Version 3, 29 June 2007.
 """
 
@@ -18,18 +18,18 @@ def create_directory_trees(args, log):
     """
     :Author: Daniel Mohr
     :Email: daniel.mohr@gmx.de
-    :Date: 2017-01-29 (last change).
+    :Date: 2017-01-29, 2021-05-23 (last change).
     :License: GNU GENERAL PUBLIC LICENSE, Version 3, 29 June 2007.
     """
     log.debug("directory trees: %s" % args.dir)
     for reldir in args.dir:
         absdir = os.path.abspath(reldir)
         absdir_hash = hashlib.sha512()
-        absdir_hash.update(absdir)
+        absdir_hash.update(absdir.encode())
         absdir_hash = base64.b32encode(absdir_hash.digest())[:-2].lower()
         log.info(" directory tree: %s" % reldir)
         config_dir = os.path.join(args.config_data_directory[0],
-                                  absdir_hash)
+                                  absdir_hash.decode())
         if not os.path.exists(config_dir):
             os.makedirs(config_dir)
         log.info(" configs: %s" % config_dir)
