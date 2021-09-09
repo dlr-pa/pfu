e7456356d289af518dbb093a3b676c48a01d5ec3
Daniel Mohr <daniel.mohr@dlr.de>
2021-09-09 11:35:45 +0200

    enhanced coding style

--- tests/script_pfu_replicate.py
+++ tests/script_pfu_replicate.py
@@ -15,13 +15,12 @@ You can run this file directly::
 Or you can run only one test, e. g.::
 
   env python3 script_pfu_replicate.py \
-    script_pfu_replicate.test_script_pfu_replicate_1
+    ScriptPfuReplicate.test_script_pfu_replicate_1
 
   pytest-3 -k test_script_pfu_replicate_1 script_pfu_replicate.py
 """
 
 import os
-import re
 import subprocess
 import tempfile
 import unittest
@@ -34,7 +33,7 @@ except (ModuleNotFoundError, ImportError):
     from checkoutput_check_checksum import checkoutput
 
 
-class script_pfu_replicate(unittest.TestCase):
+class ScriptPfuReplicate(unittest.TestCase):
     """
     :Author: Daniel Mohr
     :Date: 2021-05-25, 2021-08-31
@@ -51,21 +50,21 @@ class script_pfu_replicate(unittest.TestCase):
             self.skipTest('"pfu replicate" is only working on posix systems')
             return
         # check if sha256sum is available
-        cp = subprocess.run(
+        cpi = subprocess.run(
             'sha256sum --version',
             stdout=subprocess.PIPE, stderr=subprocess.PIPE,
             shell=True,
             timeout=23, check=False)
         extraparam = ''
-        if cp.returncode != 0:
+        if cpi.returncode != 0:
             # self.skipTest('sha256sum not available, skipping test')
             # check if sha256 is available
-            cp = subprocess.run(
+            cpi = subprocess.run(
                 'sha256 -s foo',
                 stdout=subprocess.PIPE, stderr=subprocess.PIPE,
                 shell=True,
                 timeout=23, check=False)
-            if cp.returncode != 0:
+            if cpi.returncode != 0:
                 self.skipTest(
                     'sha256sum and sha256 not available, skipping test')
                 return
@@ -88,7 +87,7 @@ class script_pfu_replicate(unittest.TestCase):
             param = '-source ' + src_dir
             param += ' -destination ' + ' '.join(dest_dirs)
             param += ' ' + extraparam
-            cp = subprocess.run(
+            subprocess.run(
                 'pfu replicate ' + param,
                 stdout=subprocess.PIPE, stderr=subprocess.PIPE,
                 shell=True,
@@ -97,12 +96,12 @@ class script_pfu_replicate(unittest.TestCase):
             for dest_dir in dest_dirs:
                 param = '-loglevel 20 -ignore_extension log status'
                 param += ' -dir ' + dest_dir
-                cp = subprocess.run(
+                cpi = subprocess.run(
                     'pfu check_checksum ' + param,
                     stdout=subprocess.PIPE, stderr=subprocess.PIPE,
                     shell=True,
                     timeout=23, check=False)
-                self.assertTrue(checkoutput(cp.stderr))
+                self.assertTrue(checkoutput(cpi.stderr))
 
 
 if __name__ == '__main__':
