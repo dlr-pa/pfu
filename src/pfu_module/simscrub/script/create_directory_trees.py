12a48a4b58e022cc0891b1e30502e566dda2796e
Daniel Mohr <daniel.mohr@dlr.de>
2023-04-21 16:04:29 +0200

    enhanced coding style

--- src/pfu_module/simscrub/script/create_directory_trees.py
+++ src/pfu_module/simscrub/script/create_directory_trees.py
@@ -21,18 +21,18 @@ def create_directory_trees(args, log):
     :Date: 2017-01-29, 2021-05-23 (last change).
     :License: GNU GENERAL PUBLIC LICENSE, Version 3, 29 June 2007.
     """
-    log.debug("directory trees: %s" % args.dir)
+    log.debug("directory trees: %s", args.dir)
     for reldir in args.dir:
         absdir = os.path.abspath(reldir)
         absdir_hash = hashlib.sha512()
         absdir_hash.update(absdir.encode())
         absdir_hash = base64.b32encode(absdir_hash.digest())[:-2].lower()
-        log.info(" directory tree: %s" % reldir)
+        log.info(" directory tree: %s", reldir)
         config_dir = os.path.join(args.config_data_directory[0],
                                   absdir_hash.decode())
         if not os.path.exists(config_dir):
             os.makedirs(config_dir)
-        log.info(" configs: %s" % config_dir)
+        log.info(" configs: %s", config_dir)
         with open(os.path.join(config_dir, 'dir'), 'wb') as file_desriptor:
             pickle.dump(reldir, file_desriptor)
         list_of_files = pfu_module.simscrub.create_file_list(reldir, log)
@@ -40,5 +40,6 @@ def create_directory_trees(args, log):
             pickle.dump(list_of_files, file_desriptor)
         with open(os.path.join(config_dir, 'status'), 'wb') as file_desriptor:
             pickle.dump(0, file_desriptor)
-        with open(os.path.join(config_dir, 'log'), 'w') as file_desriptor:
+        with open(os.path.join(config_dir, 'log'),
+                  mode='w', encoding='utf-8') as file_desriptor:
             pass
